5f5f600c6b4acb2ebe65a5217b44e41f2c175301
Gulam Sarwar <gssarwar@gmail.com>
2018-08-18 15:53:00 +0530

    sample imp

--- javaLamdaBasics/src/com/gs/lamdabasics/Greeter.java
+++ javaLamdaBasics/src/com/gs/lamdabasics/Greeter.java
@@ -1,10 +1,16 @@
 package com.gs.lamdabasics;
 
-public class Greeter {
-
-	public static void main(String[] args) {
-		
-
+public class Greeter 
+{
+	public void greet(Greeting greet)
+	{
+		greet.performs();
+	}
+	public static void main(String[] args) 
+	{
+		Greeter greeter = new Greeter();
+		HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
+		greeter.greet(helloWorldGreeting);
 	}
 
 }
