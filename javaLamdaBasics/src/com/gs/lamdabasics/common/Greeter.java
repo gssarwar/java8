917ba66e23cd50be8d9d2484775c37ee1f29647c
Gulam Sarwar <gssarwar@gmail.com>
2018-08-21 12:32:05 +0530

    dsdsc

--- javaLamdaBasics/src/com/gs/lamdabasics/common/Greeter.java
+++ javaLamdaBasics/src/com/gs/lamdabasics/common/Greeter.java
@@ -8,6 +8,7 @@ public class Greeter
 	}
 	public static void main(String[] args) 
 	{
+		System.out.println();
 		Greeter greeter = new Greeter();
 		HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
 		greeter.greet(helloWorldGreeting);
